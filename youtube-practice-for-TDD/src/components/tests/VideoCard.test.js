fdce387612818dfe1c29ae7e42f99b2e3b00383d
white-hun <121800818+white-hun@users.noreply.github.com>
2023-08-28 17:37:53 +0900

    Update VideoCard.test.js

--- youtube-practice-for-TDD/src/components/tests/VideoCard.test.js
+++ youtube-practice-for-TDD/src/components/tests/VideoCard.test.js
@@ -1,9 +1,10 @@
 import { fakeVideo as video } from "../../tests/videos";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { withRouter } from "../../tests/utils";
 import VideoCard from "../VideoCard";
-import { Route } from "react-router-dom";
+import { Route, useLocation } from "react-router-dom";
 import { formatAgo } from "../../util/date";
+import userEvent from "@testing-library/user-event";
 
 describe("VideoCard", () => {
   const { title, channelTitle, publishedAt, thumbnails } = video.snippet;
@@ -17,5 +18,26 @@ describe("VideoCard", () => {
     expect(screen.getByText(title)).toBeInTheDocument();
     expect(screen.getByText(channelTitle)).toBeInTheDocument();
     expect(screen.getByText(formatAgo(publishedAt))).toBeInTheDocument();
+  });
+
+  it("navigates to detailed video page with video state when chlicked", async () => {
+    function LocationStateDisplay() {
+      return <pre>{JSON.stringify(useLocation().state)}</pre>;
+    }
+
+    render(
+      withRouter(
+        <>
+          <Route path="/" element={<VideoCard video={video} />} />
+          <Route path={`/videos/watch/${video.id}`} element={<LocationStateDisplay />} />
+        </>
+      )
+    );
+
+    const card = screen.getByRole("listitem");
+    userEvent.click(card);
+    await waitFor(() => {
+      expect(screen.getByText(JSON.stringify({ video }))).toBeInTheDocument();
+    });
   });
 });
